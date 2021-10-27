b39798579474e1019e0fb19f8366e668e312df4a
TechXero <steve@techxero.com>
2021-10-27 22:20:27 +0300

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# xero-layan-git
-XeroLinux Layan Rice
+### This is September's Rice
+
+Some notes before proceeding :
+
+> Applying this rice will override all your settings. So make sure to create a backup of your system either via Snapper if using it, or TimeShift. Don't worry though, script will attempt to create a backup for you of your ~/.config folder before proceeding. Still better be safe than sorry.
+
+![September](https://i.imgur.com/oOOEKzw.jpeg)
+
+Rice will be compressed and made available via archive. Just extract, run terminal inside directory and run this command `./install.sh` Script will do the following steps :
+
+- Create a cbackup of your ~/.config folder.
+- Download and install necessary packages if needed...
+- Override your settings and files with this rice's.
+- Install and apply this month's Grub Theme...
+- Reboot system to get everything loaded... 
+
+> Use this at your own Risk ! It was requested, so am satisfying this request, but I won't be held liable if you didn't follow above recommendations.... Always Backup.. If you don't and break your system it's on you !!!!
+
+### Download Archive from Mega.nz
+https://mega.nz/file/xWQCxT5Z#DdnTHC3w_a5dLcKEF6EGssmxD8se5HRlaF7acWCpA4s
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#set -e
+echo "##########################################"
+echo "Be Careful this will override your Rice!! "
+echo "##########################################"
+sleep 5
+echo "Creating Backups of ~/.config folder"
+echo "#####################################"
+cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
+sleep 2
+echo "Installing required Tools"
+echo "#################################"
+sudo pacman -S --needed --noconfirm kvantum-qt5 latte-dock lolcat neofetch yakuake
+sleep 2
+echo "Installing Layan Theme"
+echo "#################################"
+sudo pacman -S --needed --noconfirm layan-cursor-theme-git layan-gtk-theme-git layan-kde-git tela-circle-icon-theme-git
+sleep 2
+echo "Installing Fonts"
+echo "#################################"
+sudo pacman -S --needed --noconfirm awesome-terminal-fonts nerd-fonts-fira-code noto-fonts-emoji otf-libertinus tex-gyre-fonts ttf-droid ttf-hack ttf-roboto
+sleep 2
+echo "Applying new Rice, hold on..."
+echo "#################################"
+cp -Rf Configs/Home/. ~
+sudo cp -Rf Configs/System/. /
+sleep 2
+echo "Applying Grub Theme...."
+echo "#################################"
+chmod +x CyberRe.sh
+sudo ./CyberRe.sh
+sleep 2
+echo "Rebooting system in 5 seconds..."
+echo "#################################"
+sleep 5
+sudo reboot
+
+
+
+
