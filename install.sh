7a764c5aba33be8a1ad7b9a038b68cdc831bc287
XeroLinux <steve@techxero.com>
2022-06-21 17:05:22 +0300

    Fixes

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ mv ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
 sleep 2
 echo "Removing old Latte-Dock & Lightly"
 echo "#################################"
-sudo pacman -Rdd --noconfirm latte-dock-git lightly-git
+sudo pacman -R --noconfirm latte-dock lightly-git
 sleep 2
 echo
 echo "Installing required Tools"
