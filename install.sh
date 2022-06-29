48f74208ec600875c7a459ffa25757e0ca527162
XeroLinux <steve@techxero.com>
2022-06-29 12:17:40 +0300

    Fixes

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ echo "Applying new Rice, hold on..."
 echo "#################################"
 cp -Rf Configs/Home/. ~
 sudo cp -Rf Configs/System/. /
+sed -i "s/{.*}/{C2}/g" ~/XeroAscii
 sudo sed -i "s/Current=.*/Current=XeroDark/" /etc/sddm.conf.d/kde_settings.conf
 sleep 2
 echo "Applying Grub Theme...."
